e01d2c8e5b2d31efb35dd516216bf15c3e06e4fe
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-11-09 20:09:43 +0200

    done login page

---
+++ src/components/LoginPage/LoginForm/LoginForm.jsx
@@ -0,0 +1,110 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import clsx from 'clsx';
+import { useState } from 'react';
+import Icon from '../../../shared/Icon/Icon';
+import css from './LoginForm.module.css';
+import { NavLink } from 'react-router-dom';
+import LoginFormSchema from '../../../helpers/Schemas/LoginFormSchema';
+
+const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isPasswordTouched, setIsPasswordTouched] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(LoginFormSchema),
+    mode: 'onSubmit',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = data => {
+    console.log(data);
+    reset();
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handlePasswordFocus = () => {
+    setIsPasswordTouched(true);
+  };
+
+  const handlePasswordBlur = () => {
+    setIsPasswordTouched(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={css.form}>
+      <div className={css.inputWrapper}>
+        <label className={css.labelWrapper}>
+          <input
+            type="email"
+            {...register('email')}
+            placeholder="Email address"
+            className={clsx(css.input, { [css.inputError]: errors.email })}
+          />
+          {errors.email && (
+            <p className={css.errorMessage}>{errors.email?.message}</p>
+          )}
+        </label>
+        <label className={css.labelWrapper}>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            {...register('password')}
+            placeholder="Password"
+            autoComplete="on"
+            onFocus={handlePasswordFocus}
+            onBlur={handlePasswordBlur}
+            className={clsx(css.input, {
+              [css.inputError]: errors.password,
+              [css.inputSuccess]: !errors.password && getValues('password'),
+            })}
+          />
+          <button
+            className={css.showPasswordBtn}
+            type="button"
+            onClick={handleClickShowPassword}
+          >
+            {showPassword ? (
+              <Icon className={css.icon} iconId="icon-eye-off" />
+            ) : (
+              <Icon className={css.icon} iconId="icon-eye" />
+            )}
+          </button>
+          {!errors.password && isPasswordTouched && getValues('password') && (
+            <>
+              <p className={clsx(css.errorMessage, css.successMessage)}>
+                Password is valid!
+              </p>
+            </>
+          )}
+          {errors.password && (
+            <>
+              <p className={css.errorMessage}>{errors.password.message}</p>
+            </>
+          )}
+        </label>
+      </div>
+      <div className={css.btnWrapper}>
+        <button type="submit" className={css.btn}>
+          Log in
+        </button>
+        <NavLink to="/login" className={css.linkLogin}>
+          Don't have an account?
+        </NavLink>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,14 @@
+import LoginForm from '../../components/LoginPage/LoginForm/LoginForm';
+import RegisterLoginBanner from '../../components/RegisterPage/RegisterLoginBanner/RegisterLoginBanner';
 import css from './LoginPage.module.css';
 
 const LoginPage = () => {
-  return <div className={css.wrapper}></div>;
+  return (
+    <div className={css.wrapper}>
+      <RegisterLoginBanner />
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
