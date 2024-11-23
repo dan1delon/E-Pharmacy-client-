709c4bfec533d3f60d8cf82e1e136bb98ea7c602
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-11-23 11:27:10 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,12 +4,14 @@ import css from './Loader.module.css';
 const Loader = () => {
   return (
     <div className={css.background}>
-      <Triangle
+      <MutatingDots
         visible={true}
-        height="120"
-        width="120"
-        color="#0081CF"
-        ariaLabel="triangle-loading"
+        height="100"
+        width="100"
+        color="#4fa94d"
+        secondaryColor="#4fa94d"
+        radius="12.5"
+        ariaLabel="mutating-dots-loading"
         wrapperStyle={{}}
         wrapperClass={css.wrapper}
       />
