ac4a86afd576934a9ac4ebef4b5d4c0a9b1eaa7d
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-11-24 13:06:25 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -10,7 +10,7 @@ const Loader = () => {
         width="120"
         color="#4fa94d"
         secondaryColor="#4fa94d"
-        radius="10"
+        radius="0"
         ariaLabel="mutating-dots-loading"
         wrapperStyle={{}}
         wrapperClass={css.wrapper}
