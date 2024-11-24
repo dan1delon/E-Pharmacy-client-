c5320823a41e9e736c344c10760093163e088731
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-11-24 13:07:49 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -6,8 +6,8 @@ const Loader = () => {
     <div className={css.background}>
       <MutatingDots
         visible={true}
-        height="120"
-        width="120"
+        height="100"
+        width="100"
         color="#4fa94d"
         secondaryColor="#4fa94d"
         radius="12.5"
